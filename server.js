eb0a9ede9017d9f043e38e514590edbc9e501c14
wdm <1285456837@qq.com>
2020-05-30 11:17:00 +0800

    first commit

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var path = require('path');
+var fs = require('fs');
 var usocket = [];
-
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-
+//开发静态资源
+app.use(express.static(path.join(__dirname, 'public')));
 io.on('connection', function(socket){
   console.log('a user connected')
 
   socket.on("join", function (name) {
+    socket.name = name;
     usocket[name] = socket
     io.emit("join", name)
   })
@@ -18,7 +22,16 @@ io.on('connection', function(socket){
   socket.on("message", function (msg) {
     io.emit("message", msg) //将新消息广播出去
   })
-
+  socket.on('disconnect', function(msg){
+    io.emit('disconnect', socket.name);
+  })
+  socket.on('base64 file', function (msg) {
+    socket.username = msg.username;
+    // socket.broadcast.emit('base64 image', //exclude sender
+    io.sockets.emit('base64 file',  //include sender
+        msg
+    );
+  });
 });
 
 http.listen(3000, function() {
